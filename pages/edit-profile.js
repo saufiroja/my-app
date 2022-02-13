cd245cb40ab2e22ef641266c8693dec1518f5d85
hsbysddq <hsby15sddq@gmail.com>
2022-02-13 20:39:49 +0700

    Membuat Asset menjadi component

--- pages/edit-profile.js
+++ pages/edit-profile.js
@@ -3,6 +3,11 @@ import { connect } from "react-redux";
 import { useSelector, useDispatch } from "react-redux";
 import { updateProfile } from "../redux/actions/users";
 import Image from 'next/image';
+import AssetKuning from "../components/Assets/AssetKuning";
+import LingkaranKuning from "../components/Assets/LingkaranKuning";
+import AssetUngu from "../components/Assets/AssetUngu";
+import AssetGame from "../components/Assets/AssetGame";
+import AssetBatu from "../components/Assets/AssetBatu";
 
 
 const EditProfile = (props) => {
@@ -24,21 +29,12 @@ const EditProfile = (props) => {
     }
     return (
         <div className="bg-white no-repeat bg-cover h-screen w-screen font-body relative overflow-hidden">
-            <div className="absolute xl:ml-[-215px] xl:mt-[-185px] xl:flex lg:ml-[-215px] lg:mt-[-185px] lg:flex md:ml-[-215px] md:mt-[-185px] md:flex sm:ml-[-215px] sm:mt-[-185px]">
-            <Image src="/images/kuning.svg" alt="impostor-logo" width={280} height={280}/>
-            </div>
-            <div className="absolute xl:left-[50%] xl:top-[20%] xl:flex lg:left-[53%] lg:top-[17%] lg:flex md:hidden sm:hidden">
-            <Image src="/images/kuning.svg" alt="impostor-logo" width={120} height={120}/>
-            </div>
-            <div className="absolute xl:top-[-12%] xl:right-[-40%] xl:flex lg:top-[-32%] lg:right-[-60%] lg:flex md:top-[-12%] md:right-[-93%] md:flex sm:hidden">
-            <Image src="/images/ungu.svg" alt="impostor-logo" width={964} height={964}/>
-            </div>
-            <div className="absolute xl:left-[70%] xl:top-[34%] xl:flex lg:left-[70%] lg:top-[34%] lg:flex md:left-[70%] md:top-[40%] md:flex sm:hidden">
-            <Image src="/images/game.svg" alt="impostor-logo" width={458} height={308}/>
-            </div>
-            <div className="absolute xl:left-[82%] xl:top-[78%] xl:flex lg:left-[82%] lg:top-[70%] lg:flex md:left-[82%] md:top-[67%] md:flex sm:hidden">
-            <Image src="/images/batu.svg" alt="impostor-logo" width={79} height={31}/>
-            </div>
+            <AssetKuning />
+            <LingkaranKuning />
+            <AssetUngu />
+            <AssetGame />
+            <AssetBatu />
+            
             <div className="container mx-auto px-[6%]">
                 <div className="h-screen w-5/12 pt-[80px] xl:ml-4 lg:ml-4 md:ml-20 sm:ml-[30%]">
                     <p className="text-[38px] text-center font-semibold text-slate-700">Edit Profile</p>
