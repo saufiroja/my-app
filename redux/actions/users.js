39930a75f0edf4d8b1a46a8079c5ed2f3bb8dd22
ulfatun_amanah <ulfaamana@gmail.com>
2022-02-13 13:02:17 +0700

    create redux for edit-profile

--- redux/actions/users.js
+++ redux/actions/users.js
@@ -1,6 +1,8 @@
 import {
+  ERROR_AUTH,
   LOGIN_USER,
-  REGISTER_USER
+  REGISTER_USER,
+  UPDATE_PROFILE
 } from "../constants/users";
 import axios from "axios";
 
@@ -8,7 +10,12 @@ export const registerUser = (payload) => async (dispatch) => {
   // loading
   dispatch({
     type: REGISTER_USER,
-    payload: false
+    payload: {
+      loading: true,
+      data: false,
+      error: false,
+      redirect: false
+    }
   })
 
   // post data
@@ -17,11 +24,25 @@ export const registerUser = (payload) => async (dispatch) => {
       console.log("success: ", res);
       dispatch({
         type: REGISTER_USER,
-        payload: res.data
+        payload: {
+          loading: false,
+          data: res.data.data,
+          error: false,
+          redirect: true
+        }
       })
     })
     .catch((err) => {
-      console.log('error: ', err);
+      console.log("error: ", err.response.data);
+      dispatch({
+        type: REGISTER_USER,
+        payload: {
+          loading: false,
+          data: false,
+          error: err.response.data,
+          redirect: false
+        }
+      })
     })
 }
 
@@ -29,7 +50,12 @@ export const loginUser = (payload) => async (dispatch) => {
   // loading
   dispatch({
     type: LOGIN_USER,
-    payload: false
+    payload: {
+      loading: true,
+      data: false,
+      error: false,
+      redirect: false
+    }
   })
 
   // post data
@@ -45,11 +71,68 @@ export const loginUser = (payload) => async (dispatch) => {
         localStorage.setItem("data", JSON.stringify(res.data));
         // localStorage.setItem("userId", res.data.data.user.id);
         // setRedirect(true);
-      } else {
-        console.log("you haven't token");
       }
+      dispatch({
+        type: LOGIN_USER,
+        payload: {
+          loading: false,
+          data: res.data.data,
+          error: false,
+          redirect: true
+        }
+      })
     })
     .catch((err) => {
-      console.log('error: ', err);
+      console.log('error: ', err.response.data.message);
+      dispatch({
+        type: LOGIN_USER,
+        payload: {
+          loading: false,
+          data: false,
+          error: err.response.data.message,
+          redirect: false
+        }
+      })
     })
+}
+
+export const updateProfile = (payload) => (dispatch) => {
+  // loading
+  dispatch({
+    type: UPDATE_PROFILE,
+    payload: {
+      loading: true,
+      data: false,
+      error: false,
+      redirect: false
+    }
+  })
+
+  axios
+    .put(`https://impostorteam-app.herokuapp.com/api/users/${payload.id}`, payload)
+    .then((res) => {
+      console.log('res update biodata:', res);
+      localStorage.setItem('data', JSON.stringify(res))
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: {
+          loading: false,
+          data: res.data,
+          error: false,
+          redirect: false
+        }
+      })
+    })
+    .catch((err) => {
+      console.log("error: ", err);
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: {
+          loading: false,
+          data: false,
+          error: err.response,
+          redirect: false
+        }
+      })
+    });
 }
