34c3316be841ce6ebbca5142d8a125ade72a71b5
saufiroja <saufiroja08@gmail.com>
2022-02-15 17:03:53 +0800

    added list game

--- components/home/list/List.jsx
+++ components/home/list/List.jsx
@@ -21,30 +21,138 @@ const List = () => {
           List Game
         </Typography>
         <Grid container spacing={4}>
-          {cards.map((card) => (
-            <Grid item key={card} xs={12} sm={6} md={4}>
-              <Card className='h-full flex flex-col'>
-                <CardMedia
-                  image='https://source.unsplash.com/random'
-                  sx={{ paddingTop: '40%' }}
-                />
-                <CardContent className='flex-grow'>
-                  <Typography variant='h5' className='font-body'>
-                    Heading
-                  </Typography>
-                  <Typography className='font-body'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Reiciendis, unde?
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button variant='outlined' size='small'>
-                    Play
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className='h-full flex flex-col'>
+              <CardMedia
+                image='./images/game1.png'
+                sx={{ paddingTop: '60%' }}
+              />
+              <CardContent className='flex-grow'>
+                <Typography variant='h5' className='font-body'>
+                  Rock Paper Scissor
+                </Typography>
+                <Typography className='font-body'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis, unde?
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button href='/game' variant='outlined' size='small'>
+                  Play
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className='h-full flex flex-col'>
+              <CardMedia
+                image='./images/game2.png'
+                sx={{ paddingTop: '60%' }}
+              />
+              <CardContent className='flex-grow'>
+                <Typography variant='h5' className='font-body'>
+                  Super Mario Car
+                </Typography>
+                <Typography className='font-body'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis, unde?
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant='outlined' size='small'>
+                  Play
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className='h-full flex flex-col'>
+              <CardMedia
+                image='./images/game3.png'
+                sx={{ paddingTop: '60%' }}
+              />
+              <CardContent className='flex-grow'>
+                <Typography variant='h5' className='font-body'>
+                  Assassin Creed
+                </Typography>
+                <Typography className='font-body'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis, unde?
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant='outlined' size='small'>
+                  Play
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className='h-full flex flex-col'>
+              <CardMedia
+                image='./images/game4.png'
+                sx={{ paddingTop: '60%' }}
+              />
+              <CardContent className='flex-grow'>
+                <Typography variant='h5' className='font-body'>
+                  Squid Game
+                </Typography>
+                <Typography className='font-body'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis, unde?
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant='outlined' size='small'>
+                  Play
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className='h-full flex flex-col'>
+              <CardMedia
+                image='./images/game5.png'
+                sx={{ paddingTop: '60%' }}
+              />
+              <CardContent className='flex-grow'>
+                <Typography variant='h5' className='font-body'>
+                  Bom Car
+                </Typography>
+                <Typography className='font-body'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis, unde?
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant='outlined' size='small'>
+                  Play
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className='h-full flex flex-col'>
+              <CardMedia
+                image='./images/game6.png'
+                sx={{ paddingTop: '60%' }}
+              />
+              <CardContent className='flex-grow'>
+                <Typography variant='h5' className='font-body'>
+                  Kena Bridge of spirits
+                </Typography>
+                <Typography className='font-body'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Reiciendis, unde?
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant='outlined' size='small'>
+                  Play
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
       </Container>
     </main>
