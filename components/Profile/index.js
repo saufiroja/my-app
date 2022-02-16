be6a5c1527d6eba4914105224b1bb7ce208278cf
hsbysddq <hsby15sddq@gmail.com>
2022-02-16 12:36:19 +0700

    Memperbaiki Tampilan

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -15,9 +15,7 @@ export default function Profile() {
             <AssetGame />
             <AssetBatu />
             <div className="container px-[6%]">
-                <div className="mt-100px">
-                <Image src="/images/feature.svg" alt="profile-picture" width={150} height={26}/>
-                </div>
+                <img class="mt-[3%]" src="/images/feature.svg" alt="" width="200" height="40"></img>
                 <div className="flex flex-row">
                     <div className="basis-1/3 h-screen">
                         <button className="bg-sky-600 hover:bg-sky-700 rounded-large px-9 py-[15px] flex flex-nowrap justify-center mt-12">
@@ -25,9 +23,9 @@ export default function Profile() {
                             <h3 className="mx-3 text-slate-50">My Profile</h3>
                         </button>
 
-                        <figure className="bg-slate-100 rounded-xl p-8 mt-6 font-body">
-                            {/* <img className="w-24 h-24 mt-3 rounded-full mx-auto" src="./images/kelinci.jpeg" alt="" width="384" height="512"/>  */}
-                            <Image src="/images/kelinci.jpeg" alt="profile-picture" width={200} height={200} className="w-24 h-24 mt-3 rounded-full mx-auto" />
+                        <figure className="bg-slate-100 rounded-xl p-8 mt-6 font-body relative">
+                            {/* <Image src="/images/kelinci.jpeg" alt="profile-picture" width={100} height={100} className="rounded-full]" /> */}
+                            <img class="w-24 h-24 rounded-full mx-auto" src="/images/kelinci.jpeg" alt="" width="384" height="512"></img>
                             <div className="pt-6 space-y-4">
                                 <figcaption className="font-medium">
                                 <div className="">
@@ -42,17 +40,17 @@ export default function Profile() {
                         </figure>
                     </div>
 
-                    <div className="basis-1/3">
-                        <div className="flex items-center w-full mr-4">
-                            <input className="focus:ring-2 focus:ring-blue-500 focus:outline-none mt-[176px] appearance-none text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-xl pl-10 ml-4 w-5/6 py-4 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Filter projects" placeholder="Search Username..."></input>
+                    <div className="basis-1/3 ml-8">
+                        <div className="flex w-full">
+                            <input className="focus:ring-2 focus:ring-blue-500 focus:outline-none mt-[126px] appearance-none text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-xl pl-10 ml-4 w-5/6 py-4 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Filter projects" placeholder="Search Username..."></input>
                         </div>
-                        <figure class="w-[66%] ml-8 md:flex rounded-xl bg-slate-100">
-                            <div class="md:p-4 text-center md:text-left">
+                        <card class="w-[66%] ml-12 md:flex rounded-xl bg-slate-100 mt-6">
+                            <div class="md:p-4 md:text-left text-center">
                                 <p class="text-lg font-medium">
                                     Saufi Roja
                                 </p>
                             </div>
-                            </figure>
+                        </card>
                     </div>
                 </div>
             </div>
